e44355b0a761e6f573662088adc9464b78c3fd32
rayjung95 <rayjung95@gmail.com>
2021-03-17 00:12:31 -0700

    add login page

---
+++ StoreManager.ExpenseReport/Models/AlertMessageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreManager.ExpenseReport.Models
+{
+    public enum AlertMessageType
+    {
+        Error, Warining, Success
+    }
+}
